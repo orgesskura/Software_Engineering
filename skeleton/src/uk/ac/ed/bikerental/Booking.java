2808220ebb4541982780df7d67d8d686f4330302
Orges Skura <s1813106@mattel.inf.ed.ac.uk>
2019-11-25 17:06:28 +0000

    added additional classes

---
+++ skeleton/src/uk/ac/ed/bikerental/Booking.java
@@ -0,0 +1,22 @@
+package uk.ac.ed.bikerental;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Booking {
+   private Quote quoteDetails;
+   private BigDecimal orderNr;
+   private BookingStatus status;
+   public Booking(Quote quote,BigDecimal order_nr,BookingStatus statuss){
+       this.quoteDetails = quote;
+       this.orderNr = order_nr;
+       this.status = statuss;
+   }
+   public Quote getQuote(){
+       return this.quoteDetails;
+   }
+   public BigDecimal getOrderNr(){
+       return this.orderNr;
+   }
+
+}
---
+++ skeleton/src/uk/ac/ed/bikerental/BookingStatus.java
@@ -0,0 +1,12 @@
+package uk.ac.ed.bikerental;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+
+enum  BookingStatus{
+   NOT_PAID,
+   PAY_DEPOSIT,
+   PAYMENT_DONE,
+   // add more types in here
+}
