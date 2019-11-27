27893805f02b9ac9c2ab67cab5e88474db23a6d3
Sami El-Daher <averagethejoe@gmail.com>
2019-11-27 00:09:41 +0000

    updated provider

--- skeleton/src/uk/ac/ed/bikerental/BikeProvider.java
+++ skeleton/src/uk/ac/ed/bikerental/BikeProvider.java
@@ -3,18 +3,33 @@ package uk.ac.ed.bikerental;
 import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.*;
+
 public class BikeProvider extends User{
   private String name;
   private Location location;
-  private ArrayList<BikeProvider> partnersWith;
-  private ArrayList<String> openingHours;
-  public BikeProvider(String nam,Location locations,ArrayList<String> openingHour,ArrayList<BikeProvider> partners){
-       super(nam,locations);
-       this.openingHours = openingHour;
+  private Set<BikeProvider> partnersWith;
+  private List<String> openingHours;
+
+  public BikeProvider(String name, Location location, List<String> openingHours, Set<BikeProvider> partners) {
+       super(name, location);
+       this.openingHours = openingHours;
        this.partnersWith = partners;
   }
 
-  //Sami...Implement the getter and setters or any other additional stuff
- 
+    public List<String> getOpeningHours() {
+	return this.openingHours;
+    }
 
+    public List<BikeProvider> getPartners() {
+	return this.partnersWith;
+    }
+
+    public void addPartner(BikeProvider partner) {
+	assert partner != null;
+
+	if (!this.contains(partner)) {
+	    this.partnersWith.add(partner);
+	    partner.addPartner(this);
+	}
+    }
 }
