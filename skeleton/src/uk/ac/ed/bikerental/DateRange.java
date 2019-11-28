73cfc31c7175e69d2621ae9d22c0c6d9ca228cea
Sami El-Daher <averagethejoe@gmail.com>
2019-11-28 20:53:55 +0000

    added javadoc comments

--- skeleton/src/uk/ac/ed/bikerental/DateRange.java
+++ skeleton/src/uk/ac/ed/bikerental/DateRange.java
@@ -13,22 +13,48 @@ public class DateRange {
 		this.end = end;
 	}
 
+	/**
+	 * Gets the date starting the range
+	 *
+	 * @return the start date for this <code>DateRange</code>
+	 */
 	public LocalDate getStart() {
 		return this.start;
 	}
 
+	/**
+	 * Gets the date ending the range
+	 *
+	 * @return the end date for this <code>DateRange</code>
+	 */
 	public LocalDate getEnd() {
 		return this.end;
 	}
 
+	/**
+	 * Gets the number of full years that are contained within the range
+	 *
+	 * @return the number of full years that has passed between the start and end date
+	 */
 	public long toYears() {
 		return ChronoUnit.YEARS.between(this.getStart(), this.getEnd());
 	}
 
+	/**
+	 * Gets the number of days that are contained within the range
+	 *
+	 * @return the number of days that have passed between the start and end date
+	 */
 	public long toDays() {
 		return ChronoUnit.DAYS.between(this.getStart(), this.getEnd());
 	}
 
+	/**
+	 * Determines whether two ranges overlap
+	 *
+	 * @return <code>true</code> if this <code>DateRange</code> has days in common
+	 * with the other <code>DateRange</code>
+	 */
 	public Boolean overlaps(DateRange other) {
 		// TODO: implement date range intersection checking
 
@@ -50,22 +76,41 @@ public class DateRange {
 				return false;
 			}
 		}
-		
+
 	}
-	// checks if this daterange lies completely on the "other" daterange
-  public Boolean overlapsCompletely(DateRange other){
-	LocalDate start1 = other.getStart();
-	LocalDate end1   = other.getEnd();
-	return start.compareTo(start1)>=0 && end.compareTo(end1)>=0;
-  }	
+
+	/**
+	 * Checks if this <code>DateRange</code> lies completely within the
+	 * <code>DateRange other</code>
+	 *
+	 * @return <code>true</code> if this <code>DateRange</code> lies completely inside the other <code>DateRange</code>
+	 */
+	public Boolean overlapsCompletely(DateRange other){
+		LocalDate start1 = other.getStart();
+		LocalDate end1   = other.getEnd();
+		return start.compareTo(start1)>=0 && end.compareTo(end1)>=0;
+	}
 
 
+	/**
+	 * Hashes the given <code>DateRange</code>
+	 *
+	 * @return the hash value for this <code>DateRange</code>
+	 */
 	@Override
 	public int hashCode() {
 		// hashCode method allowing use in collections
 		return Objects.hash(end, start);
 	}
 
+	/**
+	 * Checks equality between <code>DateRange</code> objects
+	 *
+	 * @param obj the <code>DateRange</code> to compare to this
+	 *
+	 * @return <code>true</code> if the two <code>DateRange</code> objects have the same
+	 * start and end dates
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		// equals method for testing equality in tests
--- skeleton/src/uk/ac/ed/bikerental/Location.java
+++ skeleton/src/uk/ac/ed/bikerental/Location.java
@@ -1,25 +1,62 @@
 package uk.ac.ed.bikerental;
 
+import java.util.Objects;
+
+/**
+ * <code>Location</code> class represents a real life location, and allows comparisons between
+ * other <code>Location</code> classes
+ */
 public class Location {
+	/**
+	 * <code>postcode</code> is the postcode of the <code>Location</code>
+	 */
 	private String postcode;
+
+	/**
+	 * <code>address</code> is the address of the <code>Location</code> in real life
+	 */
 	private String address;
 
+	/**
+	 * Constructs the <code>Location</code> class with all attributes specified
+	 *
+	 * @param postcode a plaintext string of the location's postcode
+	 * @param address a plaintext string of the location's address
+	 */
 	public Location(String postcode, String address) {
 		assert postcode.length() >= 6;
 		this.postcode = postcode;
 		this.address = address;
 	}
 
+	/**
+	 * Determines whether two locations are close together
+	 *
+	 * @param other the <code>Location</code> to check for closeness
+	 *
+	 * @return <code>true</code> if the <code>Location other</code> is close to <code>this</code>
+	 * @throws NullPointerException if the other <code>Location</code> is <code>null</code>
+	 */
 	public boolean isNearTo(Location other) {
-		// TODO: Implement Location.isNearTo
-		assert false;
-		return true;
+		Objects.requuireNonNull(other);
+
+		return this.postcode.equals(other.getPostcode());
 	}
 
+	/**
+	 * Gets the postcode of this location
+	 *
+	 * @return the postcode stored in the <code>Location</code> object
+	 */
 	public String getPostcode() {
 		return postcode;
 	}
 
+	/**
+	 * Gets the address of this location
+	 *
+	 * @return the address stored in the <code>Location</code> object
+	 */
 	public String getAddress() {
 		return address;
 	}
