9143f36be532d37b85073ea7f7667a198660c56d
orgesskura <orgesskura@github.com>
2019-11-29 03:22:31 +0000

    test daterange completed

--- skeleton/tests/uk/ac/ed/bikerental/TestDateRange.java
+++ skeleton/tests/uk/ac/ed/bikerental/TestDateRange.java
@@ -3,6 +3,7 @@ package uk.ac.ed.bikerental;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
+import java.time.Month;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,17 +33,39 @@ class TestDateRange {
         assertEquals(3, this.dateRange3.toYears());
     }
 
-    // @Test
-    // void testOverlapsTrue() {
-    //     // TODO: check we can see when two date ranges overlap
-    //     fail();
-    // }
+       @Test
+     void testOverlapsTrue() {
+         // TODO: check we can see when two date ranges overlap
+         LocalDate date1 = LocalDate.of(2019,Month.SEPTEMBER,21);
+         LocalDate date2 = LocalDate.of(2010,Month.MARCH,2);
+         DateRange dates1 = new DateRange(date2,date1);
+         assertTrue(dates1.overlaps(this.dateRange3));
 
-    // @Test
-    // void testOverlapsFalse() {
-    //     // TODO: check we can see when two date ranges  don't overlap
-    //     fail();
-    // }
+        }
+
+     @Test
+     void testOverlapsFalse() {
+        // TODO: check we can see when two date ranges  don't overlap
+        LocalDate date1 = LocalDate.of(2011,Month.SEPTEMBER,21);
+        LocalDate date2 = LocalDate.of(2010,Month.MARCH,2);
+        DateRange dates1 = new DateRange(date2,date1);
+        assertFalse(dates1.overlaps(this.dateRange3));
+     }
 
     // TODO: put some of your own unit tests here
+    @Test
+    void testOverlapsCompletelyTrue(){
+        LocalDate date1 = LocalDate.of(2017,Month.SEPTEMBER,21);
+        LocalDate date2 = LocalDate.of(2016,Month.MARCH,2);
+        DateRange dates1 = new DateRange(date2,date1);
+        assertTrue(dates1.overlapsCompletely(dates1)); 
+    }
+
+    @Test
+    void testOverlapsCompletelyFalse(){
+        LocalDate date1 = LocalDate.of(2020,Month.SEPTEMBER,21);
+        LocalDate date2 = LocalDate.of(2016,Month.MARCH,2);
+        DateRange dates1 = new DateRange(date2,date1);
+        assertTrue(dates1.overlapsCompletely(dates1));
+    }
 }
